ea6e8e6347e52d054159e695c74e2e628ead9457
Andreas Bombaert <a.bombaert@students.ephec.be>
2021-08-18 10:20:25 +0200

    fichiers finaux vps

--- Reseau-interne/BDD/mysql.sql
+++ Reseau-interne/BDD/mysql.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `woodytoys` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `woodytoys`;
 
 CREATE TABLE `articles` (
   `nom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -11,3 +11,6 @@ INSERT INTO `woodytoys`.`articles`
 `prix`)
 VALUES
 ("table", 40),("chaise", 20),("cheval a bascule",150),("table de chevet",25),("armoire",45);
+
+CREATE USER admin IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON woodytoys.* TO 'admin';
--- Reseau-interne/ERP/www/index.php
+++ Reseau-interne/ERP/www/index.php
@@ -1,54 +1,34 @@
-<html>
-<head>
-    <meta charset = "UTF-8">
-    <title>WoodyToys revendeurs</title>
-</head>
-    <body style="font-family: Calibri">
-    <h1> WoodyToys b2b</h1>
-    <div>
-        <h3>Mettre un article en vente</h3>
-        <form method="POST">
-            <label for="article">Article: </label>
-            <input type="text" name="article" placeholder="nom de l'article">
-            <label for="prix">Prix: </label>
-            <input type="number" name="prix" placeholder="en euro">
+<div>
+    <h3>Articles en vente</h3>
+    <?php
 
-            <input type="submit" name="submit" value="Mettre en vente">
-        </form>
+    $servername = '135.125.101.201:3306';
+    $username = 'admin';
+    $password = "password";
+    $dbname = 'woodytoys';
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    </div>
+    $result= $conn->query("SELECT * FROM articles");
 
-    <div>
-        <h3>Articles en vente</h3>
-        <?php
+    while ($row = $result->fetch()) {
+        echo $row['nom'] . " - " . $row['prix'] . "€ <br />\n";
+    }
 
-        $servername = '135.125.101.201:3306';
-        $username = 'admin';
-        $password = "password";
-        $dbname = 'woodytoys';
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (isset($_POST['submit'])){
+        $article = $_POST["article"];
+        $prix = $_POST["prix"];
 
-        $result= $conn->query("SELECT * FROM articles");
+        $insert = "INSERT INTO articles VALUES ('$article', $prix)";
+        $conn->exec($insert);
+        echo "<meta http-equiv='refresh' content='0'>";
+    }
 
-        while ($row = $result->fetch()) {
-            echo $row['nom'] . " - " . $row['prix'] . "€ <br />\n";
-        }
-
-        if (isset($_POST['submit'])){
-            $article = $_POST["article"];
-            $prix = $_POST["prix"];
-
-            $insert = "INSERT INTO articles VALUES ('$article', $prix)";
-            $conn->exec($insert);
-            header("Refresh:0");
-        }
-
-        $conn = null;
-        ?>
-    </div>
+    $conn = null;
+    ?>
+</div>
 
 
 
-    </body>
+</body>
 </html>
